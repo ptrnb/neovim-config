38fc10c00717e37b81a9d902c68956ef433f0bec
Peter Brown <peternbrown@mac.com>
2022-02-12 09:31:43 +1100

    Change colorscheme and section markers

--- lua/pbrown/lualine.lua
+++ lua/pbrown/lualine.lua
@@ -6,17 +6,26 @@ end
 lualine.setup {
   options = {
     icons_enabled = false,
-    theme = 'nightfox',
+    theme = 'nord',
     component_separators = { left = '', right = ''},
 --    section_separators = { left = '', right = ''},
 --    section_separators = { left = '█', right = '█'},
-    section_separators = { left = '', right = ''},
+    section_separators = { left = '|', right = '|' },
     disabled_filetypes = {},
     always_divide_middle = true,
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {
+      {
+          'branch',
+          icons_enabled = true,
+          icon = '',
+      },
+      {
+        'diff', 'diagnostics'
+      },
+    },
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
